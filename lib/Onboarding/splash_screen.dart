761ab958b625b8375565c9c922fd0970cf9442a4
AlaaQtishat <alaaqtishat2004@gmail.com>
2026-03-26 23:12:08 +0300

    added the themeManager, the Splash, the onboarding

--- lib/Onboarding/splash_screen.dart
+++ lib/Onboarding/splash_screen.dart
@@ -1,26 +1,73 @@
-// import 'package:flutter/material.dart';
-// import 'package:just_lost_and_found/services/Auth-service_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:just_lost_and_found/managers/theme_manager.dart';
+import 'on_boarding_screen.dart';
 
-// class SplashScreen extends StatefulWidget {
-//   const SplashScreen({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
 
-//   @override
-//   State<SplashScreen> createState() => _SplashScreenState();
-// }
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
 
-// class _SplashScreenState extends State<SplashScreen> {
-//   Future<void>navigateToNextScreen() async {
-//     await Future.delayed(Duration(seconds:2));
-//     try {
-//       bool isLoggedIn=await AuthServices.userLogin();
-      
-//     } catch (e) {
-      
-//     }
-    
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Placeholder();
-//   }
-// }
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _startSplashAndNavigate();
+  }
+
+  void _startSplashAndNavigate() {
+    Future.delayed(const Duration(seconds: 3), () {
+      if (!mounted) return;
+
+      final user = FirebaseAuth.instance.currentUser;
+
+      if (user != null) {
+        //here we will implement the code that would actually takes us inside the app rather than keep showing the onboarding.
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const OnboardingScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "images/lost_and_found_logoo.png",
+              height: 200,
+              width: 200,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              "Find what's yours,",
+              style: TextStyle(
+                fontSize: 20,
+                color: ThemeManager.primaryBlue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "return what's theirs",
+              style: TextStyle(
+                fontSize: 20,
+                color: ThemeManager.primaryBlue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(color: ThemeManager.primaryYellow),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:just_lost_and_found/Screens/Forget_password_screen.dart';
-import 'package:just_lost_and_found/Screens/SignIn_screen.dart';
+import 'package:just_lost_and_found/Onboarding/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:just_lost_and_found/Screens/SignUp_screen.dart';
-import 'package:just_lost_and_found/Screens/profile_screen.dart';
 import 'firebase_options.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main()async {
-   WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    debugPrint("Firebase is already initialized");
+  }
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  runApp(
+    const MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen()),
   );
-
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-     home: ProfilePage()));
 }
 
 class MyApp extends StatelessWidget {
