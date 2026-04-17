16b7ada46952c4020a6e2f398ea752e34bd3e90e
Razan-Abdullah-20 <roritanazzal27@gmail.com>
2026-04-17 20:59:25 +0300

    updated explore page

--- lib/screens/bottom_navigation_screens/explore_screen.dart
+++ lib/screens/bottom_navigation_screens/explore_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:just_lost_and_found/helpers/explore_options.dart';
-
 import 'package:just_lost_and_found/services/theme_manager.dart';
 
 class ExplorePage extends StatefulWidget {
@@ -16,7 +15,6 @@ class _ExplorePageState extends State<ExplorePage> {
   @override
   void initState() {
     super.initState();
-    //filteredItems = allItems;
     _focusNode.addListener(() {
       setState(() {});
     });
@@ -53,7 +51,126 @@ class _ExplorePageState extends State<ExplorePage> {
     });
   }
 
+  Widget _buildSearch() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SizedBox(
+        height: 50,
+        child: TextField(
+          cursorColor: ThemeManager.primaryBlue,
+          controller: searchController,
+          focusNode: _focusNode,
+          onChanged: searchItem,
+          decoration: InputDecoration(
+            isDense: true,
+            filled: true,
+            fillColor: const Color.fromARGB(192, 255, 255, 255),
+            hintText: "Search for items...",
+            hintStyle: TextStyle(fontSize: 15, color: Colors.grey),
+            prefixIcon: Icon(
+              Icons.search,
+              color: ThemeManager.primaryBlue,
+              size: 25,
+            ),
+            suffixIcon: searchController.text.isNotEmpty
+                ? IconButton(
+                    icon: const Icon(Icons.clear),
+                    onPressed: () {
+                      searchController.clear();
+                      searchItem("");
+                    },
+                  )
+                : null,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(17),
+              borderSide: const BorderSide(color: Colors.white, width: 0.1),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(17),
+              borderSide: const BorderSide(color: Colors.grey, width: 0.1),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(17),
+              borderSide: const BorderSide(
+                color: ThemeManager.primaryBlue,
+                width: 1.5,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  String selectedType = "Categories";
+
   int selectedCategory = 0;
+
+  Widget _buildCategoryList() {
+    return SizedBox(
+      height: 55,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.symmetric(vertical: 5),
+        itemCount: Categories.categories.length,
+        itemBuilder: (context, index) {
+          final isSelected = selectedCategory == index;
+
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedCategory = index;
+              });
+            },
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    offset: const Offset(0, 6),
+                    blurRadius: 8,
+                    spreadRadius: -2,
+                  ),
+                ],
+                color: isSelected
+                    ? ThemeManager.primaryYellow
+                    : const Color.fromARGB(192, 255, 255, 255),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                Categories.categories[index],
+                style: TextStyle(
+                  color: isSelected ? Colors.white : Colors.black,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildLocationList() {
+    return Center(child: Text("List"));
+  }
+
+  Widget _buildPosts() {
+    return Expanded(
+      child: filteredItems.isEmpty
+          ? const Center(
+              child: Text("No results", style: TextStyle(color: Colors.grey)),
+            )
+          : ListView.builder(
+              itemCount: filteredItems.length,
+              itemBuilder: (context, index) {
+                return Card(child: ListTile(title: Text(filteredItems[index])));
+              },
+            ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,118 +178,87 @@ class _ExplorePageState extends State<ExplorePage> {
       resizeToAvoidBottomInset: false,
       body: Column(
         children: [
-          SizedBox(height: 10),
-          SizedBox(
-            width: 335,
-            height: 50,
-            child: TextField(
-              cursorColor: ThemeManager.primaryBlue,
-              controller: searchController,
-              focusNode: _focusNode,
-              onChanged: searchItem,
-              decoration: InputDecoration(
-                isDense: true,
-                filled: true,
-                fillColor: _focusNode.hasFocus
-                    ? const Color(0xFFF0F0F9)
-                    : const Color.fromARGB(255, 224, 224, 233),
-                hintText: "Search...",
-                hintStyle: TextStyle(fontSize: 15),
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: _focusNode.hasFocus
-                      ? ThemeManager.primaryBlue
-                      : Colors.grey[700],
-                ),
-                suffixIcon: searchController.text.isNotEmpty
-                    ? IconButton(
-                        icon: const Icon(Icons.clear),
-                        onPressed: () {
-                          searchController.clear();
-                          searchItem("");
-                        },
-                      )
-                    : null,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: const BorderSide(color: Colors.grey, width: 1.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: const BorderSide(
-                    color: ThemeManager.primaryBlue,
-                    width: 2.0,
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-          SizedBox(height: 5),
-
-          SizedBox(
-            height: 55,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              padding: EdgeInsets.symmetric(vertical: 5),
-              itemCount: Categories.categories.length,
-              itemBuilder: (context, index) {
-                final isSelected = selectedCategory == index;
-
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      selectedCategory = index;
-                    });
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 8),
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: const Color.fromARGB(255, 171, 170, 170),
-                          blurRadius: 5,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                      color: isSelected
-                          ? ThemeManager.primaryYellow
-                          : const Color(0xFFF0F0F9),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      Categories.categories[index],
-                      style: TextStyle(
-                        color: isSelected ? Colors.white : Colors.black,
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-
           SizedBox(height: 20),
 
-          Expanded(
-            child: filteredItems.isEmpty
-                ? const Center(
-                    child: Text(
-                      "No results",
-                      style: TextStyle(color: Colors.grey),
+          _buildSearch(),
+
+          SizedBox(height: 15),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                const Text(
+                  "Browse by  ",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+
+                PopupMenuButton<String>(
+                  onSelected: (String value) {
+                    setState(() {
+                      selectedType = value;
+                    });
+                  },
+                  itemBuilder: (BuildContext context) =>
+                      <PopupMenuEntry<String>>[
+                        const PopupMenuItem<String>(
+                          value: 'categories',
+                          child: Text('Categories'),
+                        ),
+                        const PopupMenuItem<String>(
+                          value: 'location',
+                          child: Text('Location'),
+                        ),
+                      ],
+
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 6,
                     ),
-                  )
-                : ListView.builder(
-                    itemCount: filteredItems.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: ListTile(title: Text(filteredItems[index])),
-                      );
-                    },
+                    decoration: BoxDecoration(
+                      color: ThemeManager.primaryYellow,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          selectedType,
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Icon(
+                          Icons.arrow_drop_down,
+                          size: 18,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
                   ),
+                ),
+              ],
+            ),
           ),
+
+          SizedBox(height: 10),
+
+          SizedBox(
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              child: selectedType.toLowerCase() == 'categories'
+                  ? _buildCategoryList()
+                  : _buildLocationList(),
+            ),
+          ),
+
+          SizedBox(height: 15),
+
+          _buildPosts(),
         ],
       ),
     );
