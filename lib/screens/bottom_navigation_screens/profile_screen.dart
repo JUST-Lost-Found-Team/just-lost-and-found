b267e8e9885f85262393b2920a06dbf51fd1acbd
AlaaQtishat <alaaqtishat2004@gmail.com>
2026-05-17 20:12:44 +0300

    implemented the profile screen localizations

--- lib/screens/bottom_navigation_screens/profile_screen.dart
+++ lib/screens/bottom_navigation_screens/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easy_localization/easy_localization.dart' hide TextDirection;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -21,7 +22,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
   final ImagePicker _picker = ImagePicker();
   XFile? _imageFile;
   bool isUploading = false;
-  String? _firestoreProfileImageUrl = FirebaseAuth.instance.currentUser?.photoURL;
+  String? _firestoreProfileImageUrl =
+      FirebaseAuth.instance.currentUser?.photoURL;
 
   @override
   void initState() {
@@ -39,7 +41,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
             .get();
         if (doc.exists && mounted) {
           setState(() {
-            _firestoreProfileImageUrl = (doc.data() as Map<String, dynamic>)['profileImage'];
+            _firestoreProfileImageUrl =
+                (doc.data() as Map<String, dynamic>)['profileImage'];
           });
         }
       } catch (e) {
@@ -50,14 +53,20 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   Future<void> _refreshUser() async {
     await user?.reload();
-    setState(() { user = FirebaseAuth.instance.currentUser; });
+    setState(() {
+      user = FirebaseAuth.instance.currentUser;
+    });
   }
 
   Future<void> _pickImage() async {
     try {
-      final XFile? selectedImage = await _picker.pickImage(source: ImageSource.gallery);
+      final XFile? selectedImage = await _picker.pickImage(
+        source: ImageSource.gallery,
+      );
       if (selectedImage != null) {
-        setState(() { _imageFile = selectedImage; });
+        setState(() {
+          _imageFile = selectedImage;
+        });
         await uploadToCloudinary();
       }
     } catch (e) {
@@ -67,27 +76,43 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   Future<void> uploadToCloudinary() async {
     if (_imageFile == null) return;
-    setState(() { isUploading = true; });
+    setState(() {
+      isUploading = true;
+    });
     try {
       Cloudinary cloudinary = Cloudinary();
-      String? imageUrl = await cloudinary.uploadToCloudinary(File(_imageFile!.path));
+      String? imageUrl = await cloudinary.uploadToCloudinary(
+        File(_imageFile!.path),
+      );
       if (imageUrl != null && user != null) {
         await user!.updatePhotoURL(imageUrl);
         await user!.reload();
         await _refreshUser();
-        await FirebaseFirestore.instance.collection('users').doc(user!.uid).update({'profileImage': imageUrl});
-        if (mounted) { setState(() { _firestoreProfileImageUrl = imageUrl; }); }
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user!.uid)
+            .update({'profileImage': imageUrl});
+        if (mounted) {
+          setState(() {
+            _firestoreProfileImageUrl = imageUrl;
+          });
+        }
       }
     } catch (e) {
       print("Upload error: $e");
     } finally {
-      if (mounted) { setState(() { isUploading = false; }); }
+      if (mounted) {
+        setState(() {
+          isUploading = false;
+        });
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final String userName = user?.displayName ?? "Student Name";
+    final String userName =
+        user?.displayName ?? "profile_screen.student_name".tr();
     final String userEmail = user?.email ?? "email@just.edu.jo";
 
     return Scaffold(
@@ -95,17 +120,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: LayoutBuilder(
         builder: (context, constraints) {
           return SingleChildScrollView(
-         
             child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: constraints.maxHeight, 
-              ),
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
               child: IntrinsicHeight(
-              
                 child: Stack(
                   clipBehavior: Clip.none,
                   children: [
-                    
                     Column(
                       children: [
                         Container(
@@ -120,33 +140,52 @@ class _ProfileScreenState extends State<ProfileScreen> {
                           //         fontSize: 22,
                           //         fontWeight: FontWeight.bold)),
                         ),
-                        
+
                         Expanded(
                           child: Container(
                             width: double.infinity,
                             color: Colors.white,
                             child: Column(
                               children: [
-                                const SizedBox(height: 95), 
-                                Text(userName,
-                                    style: const TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.bold,
-                                        color: ThemeManager.primaryBlue)),
-                                Text(userEmail,
-                                    style: const TextStyle(color: Colors.grey)),
+                                const SizedBox(height: 95),
+                                Text(
+                                  userName,
+                                  style: const TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: ThemeManager.primaryBlue,
+                                  ),
+                                ),
+                                Text(
+                                  userEmail,
+                                  style: const TextStyle(color: Colors.grey),
+                                ),
 
                                 const SizedBox(height: 30),
-                                _buildSectionHeader("My Posts"),
-                                _buildMenuItem(Icons.post_add, "View All", () {
-                                  Navigator.push(context, MaterialPageRoute(builder: (context) => const MyPostsScreen()));
-                                }),
+                                _buildSectionHeader(
+                                  "profile_screen.my_posts".tr(),
+                                ),
+                                _buildMenuItem(
+                                  Icons.post_add,
+                                  "profile_screen.view_all".tr(),
+                                  () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            const MyPostsScreen(),
+                                      ),
+                                    );
+                                  },
+                                ),
 
                                 const SizedBox(height: 20),
-                                _buildSectionHeader("Settings"),
+                                _buildSectionHeader(
+                                  "profile_screen.settings".tr(),
+                                ),
                                 _buildSettingsBox(),
 
-                                const Spacer(), 
+                                const Spacer(),
                                 const SizedBox(height: 20),
                               ],
                             ),
@@ -155,14 +194,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       ],
                     ),
 
-                   
                     Positioned(
                       top: 180 - 100,
                       left: 0,
                       right: 0,
-                      child: Center(
-                        child: _buildProfileImage(85),
-                      ),
+                      child: Center(child: _buildProfileImage(85)),
                     ),
                   ],
                 ),
@@ -174,7 +210,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
- 
   Widget _buildProfileImage(double radius) {
     return Stack(
       alignment: Alignment.bottomRight,
@@ -184,31 +219,61 @@ class _ProfileScreenState extends State<ProfileScreen> {
             shape: BoxShape.circle,
             border: Border.all(color: ThemeManager.primaryYellow, width: 8),
             boxShadow: [
-              BoxShadow(color: Colors.black.withOpacity(0.1), blurRadius: 10, offset: const Offset(0, 5))
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10,
+                offset: const Offset(0, 5),
+              ),
             ],
           ),
           child: CircleAvatar(
             radius: radius,
             backgroundColor: Colors.grey[300],
             child: isUploading
-                ? const CircularProgressIndicator(color: ThemeManager.primaryBlue)
+                ? const CircularProgressIndicator(
+                    color: ThemeManager.primaryBlue,
+                  )
                 : _imageFile != null
-                    ? ClipOval(child: Image.file(File(_imageFile!.path), width: radius * 2, height: radius * 2, fit: BoxFit.cover))
-                    : (_firestoreProfileImageUrl != null && _firestoreProfileImageUrl!.isNotEmpty)
-                        ? ClipOval(child: CachedNetworkImage(imageUrl: _firestoreProfileImageUrl!, width: radius * 2, height: radius * 2, fit: BoxFit.cover, placeholder: (context, url) => const CircularProgressIndicator(), errorWidget: (context, url, error) => const Icon(Icons.person, size: 60)))
-                        : const Icon(Icons.person, size: 60, color: Colors.grey),
+                ? ClipOval(
+                    child: Image.file(
+                      File(_imageFile!.path),
+                      width: radius * 2,
+                      height: radius * 2,
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                : (_firestoreProfileImageUrl != null &&
+                      _firestoreProfileImageUrl!.isNotEmpty)
+                ? ClipOval(
+                    child: CachedNetworkImage(
+                      imageUrl: _firestoreProfileImageUrl!,
+                      width: radius * 2,
+                      height: radius * 2,
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) =>
+                          const CircularProgressIndicator(),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.person, size: 60),
+                    ),
+                  )
+                : const Icon(Icons.person, size: 60, color: Colors.grey),
           ),
         ),
         GestureDetector(
           onTap: _pickImage,
           child: Container(
-            height: 40, width: 40,
+            height: 40,
+            width: 40,
             decoration: BoxDecoration(
               color: ThemeManager.primaryYellow,
               shape: BoxShape.circle,
               border: Border.all(color: Colors.white, width: 3),
             ),
-            child: const Icon(Icons.edit, color: ThemeManager.primaryBlue, size: 20),
+            child: const Icon(
+              Icons.edit,
+              color: ThemeManager.primaryBlue,
+              size: 20,
+            ),
           ),
         ),
       ],
@@ -217,20 +282,44 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   Widget _buildSectionHeader(String title) {
     return Padding(
-      padding: const EdgeInsets.only(left: 30, bottom: 8),
-      child: Align(alignment: Alignment.centerLeft, child: Text(title, style: const TextStyle(color: ThemeManager.primaryBlue, fontSize: 18, fontWeight: FontWeight.bold))),
+      padding: const EdgeInsetsDirectional.only(start: 30, bottom: 8),
+      child: Align(
+        alignment: AlignmentDirectional.centerStart,
+        child: Text(
+          title,
+          style: const TextStyle(
+            color: ThemeManager.primaryBlue,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 
   Widget _buildMenuItem(IconData icon, String title, VoidCallback onTap) {
+    bool isRtl = Directionality.of(context) == TextDirection.rtl;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15),
       child: Container(
-        decoration: BoxDecoration(color: Colors.grey[200], borderRadius: BorderRadius.circular(30)),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(30),
+        ),
         child: ListTile(
-          title: Text(title, style: const TextStyle(color: Colors.black, fontWeight: FontWeight.w500)),
+          title: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
           leading: Icon(icon, color: Colors.black),
-          trailing: const Icon(Icons.arrow_forward_ios, color: ThemeManager.primaryYellow, size: 18),
+          trailing: Icon(
+            isRtl ? Icons.arrow_forward_ios : Icons.arrow_back_ios,
+            color: ThemeManager.primaryYellow,
+            size: 18,
+          ),
           onTap: onTap,
         ),
       ),
@@ -241,29 +330,67 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15),
       child: Container(
-        decoration: BoxDecoration(color: Colors.grey[200], borderRadius: BorderRadius.circular(30)),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(30),
+        ),
         child: Column(
           children: [
-            _buildListTile("Language", Icons.language, () {}),
-            const Divider(height: 0, thickness: 0.5, endIndent: 20, indent: 20),
-            ListTile(
-              title: const Text("Light Mode"),
-              leading: const Icon(Icons.wb_sunny_outlined, color: ThemeManager.primaryYellow),
-              trailing: Switch(value: true, activeColor: ThemeManager.primaryYellow, onChanged: (val) {}),
+            _buildListTile(
+              context,
+              "profile_screen.language".tr(),
+              Icons.language,
+              () {},
             ),
             const Divider(height: 0, thickness: 0.5, endIndent: 20, indent: 20),
-            _buildListTile("Log Out", Icons.logout, () => _showLogoutDialog(context), isRed: true),
+            ListTile(
+              title: Text("profile_screen.light_mode".tr()),
+              leading: const Icon(
+                Icons.wb_sunny_outlined,
+                color: ThemeManager.primaryYellow,
+              ),
+              trailing: Switch(
+                value: true,
+                activeColor: ThemeManager.primaryYellow,
+                onChanged: (val) {},
+              ),
+            ),
+            Divider(height: 0, thickness: 0.5, endIndent: 20, indent: 20),
+            _buildListTile(
+              context,
+              "profile_screen.log_out".tr(),
+              Icons.logout,
+              () => _showLogoutDialog(context),
+              isRed: true,
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildListTile(String title, IconData icon, VoidCallback onTap, {bool isRed = false}) {
+  Widget _buildListTile(
+    BuildContext context,
+    String title,
+    IconData icon,
+    VoidCallback onTap, {
+    bool isRed = false,
+  }) {
+    bool isRtl = Directionality.of(context) == TextDirection.rtl;
     return ListTile(
-      title: Text(title, style: TextStyle(color: isRed ? Colors.red : Colors.black, fontWeight: FontWeight.w500)),
+      title: Text(
+        title,
+        style: TextStyle(
+          color: isRed ? Colors.red : Colors.black,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
       leading: Icon(icon, color: isRed ? Colors.red : Colors.black),
-      trailing: const Icon(Icons.arrow_forward_ios, color: ThemeManager.primaryYellow, size: 18),
+      trailing: Icon(
+        isRtl ? Icons.arrow_forward_ios : Icons.arrow_back_ios,
+        color: ThemeManager.primaryYellow,
+        size: 18,
+      ),
       onTap: onTap,
     );
   }
@@ -273,14 +400,36 @@ class _ProfileScreenState extends State<ProfileScreen> {
       context: context,
       builder: (context) => AlertDialog(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        title: const Row(children: [Icon(Icons.logout, color: ThemeManager.errorRed), SizedBox(width: 9), Text('Log Out')]),
-        content: const Text("Are you sure you want to log out?"),
+        title: Row(
+          children: [
+            Icon(Icons.logout, color: ThemeManager.errorRed),
+            SizedBox(width: 9),
+            Text("profile_screen.log_out".tr()),
+          ],
+        ),
+        content: Text("profile_screen.logout_confirmation".tr()),
         actions: [
-          TextButton(onPressed: () => Navigator.pop(context), child: const Text('Cancel')),
-          TextButton(onPressed: () async {
-            await FirebaseAuth.instance.signOut();
-            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const LoginScreen()), (r) => false);
-          }, child: const Text('Log Out', style: TextStyle(color: Colors.red))),
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text(
+              "profile_screen.cancel".tr(),
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+                (r) => false,
+              );
+            },
+            child: Text(
+              "profile_screen.log_out".tr(),
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
         ],
       ),
     );
